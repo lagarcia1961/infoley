63b83f8e994f4f39e3bd0dad4c36b64fc746a2d6
Hector Dominguez <hrdominguez89@gmail.com>
2025-03-14 12:38:02 -0300

    se agrega clase is granted

--- src/Controller/NormaController.php
+++ src/Controller/NormaController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\SecurityBundle\Attribute\IsGranted;
 
 #[Route('/norma')]
 class NormaController extends AbstractController
--- src/Controller/Secure/NormaController.php
+++ src/Controller/Secure/NormaController.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 #[Route('/secure/norma')]
 class NormaController extends AbstractController
